4e1c9f0a9765b142c6901faec5f39a9b450757a9
Misbah Haque <misbahmuneer2003@gmail.com>
2025-04-24 21:42:08 +0500

    Update

--- milife/lib/Services/Less.dart
+++ milife/lib/Services/Less.dart
@@ -9,11 +9,20 @@ class LessIsMore extends StatefulWidget {
 
 class _LessIsMoreState extends State<LessIsMore> {
   final List<String> quotes = [
-    "Success is not final, failure is not fatal.",
+    "Dirty Deeds Done Dirt Cheap",
     "Believe you can and you're halfway there.",
-    "Your time is limited, don’t waste it.",
+    "To defeat Evil, I shal become an even greater Evil",
     "The best time to plant a tree was 20 years ago.",
-    "What a fucking Nigger",
+    "What a fucking Nigger...",
+    "We were Pokemons all this time, waiting for people to choose us.",
+    "You're a lady as ephemeral and elegant as a lotus blossom.",
+    "Feel what you need to feel, then let it go. Don't let it consume you.",
+    "And I knew exactly what to do, but in a much more real sense, I had no idea what to do.",
+    "Ohh Little one, I don't wanna admit to something, if all it's gonna cause is pain; Truth in my lies right now are falling like the rain, so let the river run.",
+    "Softly let's have a break.",
+    "Love made me feel I knew the answer, but when I raised my hand I was the only one in the room.",
+    "Someone has to leave first. This is a very old story. There is no other version of it.",
+    "Remember kids, Don't use drugs.",
   ];
 
   String currentQuote = "";
